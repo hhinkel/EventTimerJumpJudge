353ac4303a0c04b017d08542185ae10c672066fd
hhinkel <ad-astra@comcast.net>
2019-12-09 21:56:01 -0500

    Gradle

--- app/src/main/java/com/example/jumpjudge/MainActivity.java
+++ app/src/main/java/com/example/jumpjudge/MainActivity.java
@@ -1,10 +1,25 @@
 package com.example.jumpjudge;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Global Variables
+    Button[] btn = new Button[13];
+    EditText userInput;
+    ConstraintLayout relativeLayout;
+
+    private Spinner divisionSpinner;
+    private String division;
+
+    private static final int PERMISSION_REQUEST_CODE = 100;
+    private boolean permissionGranted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
